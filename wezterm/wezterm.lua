0e98d9da904531fb12dec1bc0f8a3f62a320f2c6
Zeke Fralish <zfralish@anaconda.com>
2024-09-30 10:54:53 -0500

    tracking wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,167 @@
+local wezterm = require("wezterm")
+local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+local Keys = {}
+-- you can put the rest of your Wezterm config here
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = true,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
+	force_reverse_video_cursor = true,
+	font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
+	default_prog = { "/bin/zsh", "-l" },
+	font_size = 14.0,
+	color_scheme = "nord",
+	inactive_pane_hsb = {
+		saturation = 1,
+		brightness = 1,
+	},
+}
+function Keys.setup(config)
+	smart_splits.apply_to_config(config, {
+		-- the default config is here, if you'd like to use the default keys,
+		-- you can omit this configuration table parameter and just use
+		-- smart_splits.apply_to_config(config)
+
+		-- directional keys to use in order of: left, down, up, right
+		direction_keys = { "j", "k", "l", "'" },
+		-- modifier keys to combine with direction_keys
+		modifiers = {
+			move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
+			resize = "META", -- modifier to use for pane resize, e.g. META+h to resize to the left
+		},
+	})
+	config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+	config.disable_default_key_bindings = false
+	config.hyperlink_rules = wezterm.default_hyperlink_rules()
+	config.mouse_bindings = {
+		-- Ctrl-click will open the link under the mouse cursor
+		{
+			event = { Up = { streak = 1, button = "Left" } },
+			mods = "CTRL",
+			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
+	}
+	config.keys = {
+		{
+			key = "p",
+			mods = "LEADER",
+			action = wezterm.action.ActivateCommandPalette,
+		},
+		{
+			key = "h",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "l",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "k",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "j",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Down"),
+		},
+		{
+			key = "LeftArrow",
+			mods = "LEADER",
+			action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+		},
+		{
+			key = "DownArrow",
+			mods = "LEADER",
+			action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+		},
+		{
+			key = "UpArrow",
+			mods = "LEADER",
+			action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+		},
+		{
+			key = "RightArrow",
+			mods = "LEADER",
+			action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+		},
+		{
+			key = [[\]],
+			mods = "LEADER",
+			action = wezterm.action({
+				SplitHorizontal = { domain = "CurrentPaneDomain" },
+			}),
+		},
+		{
+			key = [[|]],
+			mods = "LEADER",
+			action = wezterm.action.SplitPane({
+				top_level = true,
+				direction = "Right",
+				size = { Percent = 50 },
+			}),
+		},
+		{
+			key = [[-]],
+			mods = "LEADER",
+			action = wezterm.action({
+				SplitVertical = { domain = "CurrentPaneDomain" },
+			}),
+		},
+		{
+			key = [[_]],
+			mods = "LEADER",
+			action = wezterm.action.SplitPane({
+				top_level = true,
+				direction = "Down",
+				size = { Percent = 25 },
+			}),
+		},
+		{
+			key = "n",
+			mods = "ALT",
+			action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }),
+		},
+		{
+			key = "p",
+			mods = "ALT|SHIFT",
+			action = wezterm.action.ShowLauncherArgs({
+				flags = "LAUNCH_MENU_ITEMS|FUZZY|TABS|DOMAINS|WORKSPACES",
+			}),
+		},
+		{
+			key = "Q",
+			mods = "ALT",
+			action = wezterm.action({ CloseCurrentTab = { confirm = false } }),
+		},
+		{ key = "q", mods = "ALT", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
+		{ key = "F11", mods = "", action = wezterm.action.ToggleFullScreen },
+		{ key = "[", mods = "ALT", action = wezterm.action({ ActivateTabRelative = -1 }) },
+		{ key = "]", mods = "ALT", action = wezterm.action({ ActivateTabRelative = 1 }) },
+		{ key = "{", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(-1) },
+		{ key = "}", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(1) },
+		{ key = "y", mods = "ALT", action = wezterm.action.ActivateCopyMode },
+		{ key = "c", mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
+		{ key = "v", mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+		{ key = "z", mods = "ALT", action = wezterm.action.TogglePaneZoomState },
+		{ key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
+		{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
+		{ key = "1", mods = "ALT", action = wezterm.action({ ActivateTab = 0 }) },
+		{ key = "2", mods = "ALT", action = wezterm.action({ ActivateTab = 1 }) },
+		{ key = "3", mods = "ALT", action = wezterm.action({ ActivateTab = 2 }) },
+		{ key = "4", mods = "ALT", action = wezterm.action({ ActivateTab = 3 }) },
+		{ key = "5", mods = "ALT", action = wezterm.action({ ActivateTab = 4 }) },
+		{ key = "6", mods = "ALT", action = wezterm.action({ ActivateTab = 5 }) },
+		{ key = "7", mods = "ALT", action = wezterm.action({ ActivateTab = 6 }) },
+		{ key = "8", mods = "ALT", action = wezterm.action({ ActivateTab = 7 }) },
+		{ key = "9", mods = "ALT", action = wezterm.action({ ActivateTab = 8 }) },
+	}
+end
+
+-- return keys and mouse
+--
+Keys.setup(config)
+return config
